4a09867cad68bd3c8841c48ef91fdeddd72007ec
elchache66 <80396896+elchache66@users.noreply.github.com>
2021-07-28 16:11:05 +0200

    Update server.js
    Ver espacios

--- server.js
+++ server.js
@@ -250,7 +250,8 @@ function oauthFlowCompleted(access_token, res) {
   // Retreive user name: GET https://api.ciscospark.com/v1/people/me
   const options = {
     method: "GET",
-    url: "https://api.ciscospark.com/v1/people/me",
+    //  url: "https://api.ciscospark.com/v1/people/me",
+    url: "https://webexapis.com/v1/rooms",
     headers: {
       authorization: "Bearer " + access_token,
     },
